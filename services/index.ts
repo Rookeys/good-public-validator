c100070c5a02d92c5694f666b84fa6d3d04edecf
Jeongmin Lee <clcl6084@gmail.com>
2024-04-14 06:23:38 +0900

    feat: implement axios and fetch function

---
+++ services/index.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+export const getTranslateBadToGood = async (text: string) => {
+  const response = await axios
+    .post("http://localhost:8000/bad2good", {
+      text,
+    })
+    .then((res) => res.data);
+
+  return response;
+};
+
+export const getChecklist = async (text: string) => {
+  const response = await axios
+    .post("http://localhost:8000/check_list", {
+      text,
+    })
+    .then((res) => res.data);
+
+  return response;
+};
+
+export const getSummary = async (text: string) => {
+  const response = await axios
+    .post("http://localhost:8000/summarize", {
+      text,
+    })
+    .then((res) => res.data);
+
+  return response;
+};
+
+export const getWhyBadRequest = async (text: string) => {
+  const response = await axios
+    .post("http://localhost:8000/why_bad_request", {
+      text,
+    })
+    .then((res) => res.data);
+
+  return response;
+};
