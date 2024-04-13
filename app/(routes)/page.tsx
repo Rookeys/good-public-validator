b8ec74d0f0f5f1157868f12b678e19f40fe41524
Jeongmin Lee <clcl6084@gmail.com>
2024-04-13 23:44:52 +0900

    feat: implement stepper component

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,7 @@
-import { Header } from '@/components/common/header';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { Header } from "@/components/common/header";
+import Stepper from "@/components/common/stepper";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -9,13 +10,16 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="flex gap-4 p-4">
-      <Button>Test</Button>
-      <Button variant={"outline"}>Test</Button>
-      <Button variant={"secondary"}>Test</Button>
-      <Button variant={"ghost"}>Test</Button>
-      <Input />
-      <Input variant={'gray'}/>
-    </div>
+    <>
+      <div className="flex gap-4 p-4">
+        <Button>Test</Button>
+        <Button variant={"outline"}>Test</Button>
+        <Button variant={"secondary"}>Test</Button>
+        <Button variant={"ghost"}>Test</Button>
+        <Input />
+        <Input variant={"gray"} />
+      </div>
+      <Stepper step={1} />
+    </>
   );
 }
